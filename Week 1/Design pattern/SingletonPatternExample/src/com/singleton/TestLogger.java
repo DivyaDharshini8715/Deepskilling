7290858cac3d11bf20d4d5e0345781e3a8114f65
Divya-87 <malarselvaraj1@gmail.com>
2026-06-20 20:54:33 +0530

    Added

---
+++ Week 1/Design pattern/SingletonPatternExample/src/com/singleton/TestLogger.java
@@ -0,0 +1,20 @@
+package com.singleton;
+
+public class TestLogger {
+
+	public static void main(String[] args) {
+		 Logger logger1 = Logger.getInstance();
+	        Logger logger2 = Logger.getInstance();
+
+	        logger1.log("First message");
+	        logger2.log("Second message");
+
+	        if (logger1 == logger2) {
+	            System.out.println("Only one Logger instance is created.");
+	        } else {
+	            System.out.println("Different Logger instances are created.");
+	        }
+
+	}
+
+}
